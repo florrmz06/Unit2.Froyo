377a404f07e205395ec779bf303d2e1322f952ab
florrmz06 <florrmz06@yahoo.com>
2023-11-03 19:26:10 -0500

    Work done friday pt.2

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const input = prompt("Please insert desired flavors, separated by commas. Available flavors: Strawberry, Coffee, Cheesecake, Vanilla")
 
-function splitInput () {
-    const input = prompt("Please insert desired flavors, separated by commas. Available flavors: Strawberry, Coffee, Cheesecake, Vanilla")
+if (input) {
+
+    const arrayOfFlavors = input.split(" ");
+
+    console.table(arrayOfFlavors);
+
+    const flavorCount = {};
+    for (let i=0; i < arrayOfFlavors.length; i++) {
+        const flavors =arrayOfFlavors[i];
+        if (flavorCount [flavors]) {
+            flavorCount[flavors]++;
+        } else {
+            flavorCount[flavors] = 1;
+        }
+    }
+
+console.log("Flavor Orders: ");
+console.table(flavorCount);
+} else {
+    console.log("No orders for this flavor");
 }
+
